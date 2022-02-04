8999e7aa0ec61947f2f723d390d4fbb0c28bd6eb
Debuggingss <55459283+Debuggingss@users.noreply.github.com>
2022-02-04 02:29:37 +0100

    Added properly made packet event.

--- src/main/kotlin/dev/debuggings/examplemod/ExampleMod.kt
+++ src/main/kotlin/dev/debuggings/examplemod/ExampleMod.kt
@@ -2,9 +2,13 @@ package dev.debuggings.examplemod
 
 import dev.debuggings.examplemod.commands.ConfigCommand
 import dev.debuggings.examplemod.core.Config
+import dev.debuggings.examplemod.events.packet.PacketEvent
+import dev.debuggings.examplemod.events.packet.PacketListener
 import net.minecraft.client.Minecraft
+import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod(
     modid = ExampleMod.MOD_ID,
@@ -29,5 +33,7 @@ class ExampleMod {
         config?.preload()
 
         ConfigCommand().register()
+
+        MinecraftForge.EVENT_BUS.register(PacketListener())
     }
 }
